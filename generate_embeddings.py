5175c6c4a893efaa426657650f4c265b03e99916
Ahlam Raiteb <araiteb@e3r2p2.1337.ma>
2024-09-17 13:05:26 +0100

    building a chat rag AI For PDF using Python

--- generate_embeddings.py
+++ generate_embeddings.py
@@ -5,18 +5,34 @@ from langchain_community.document_loaders import PyPDFLoader
 
 
 loaders = [PyPDFLoader('./report.pdf')]
-
 docs = []
 
 for file in loaders:
     docs.extend(file.load())
 
+
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
 docs = text_splitter.split_documents(docs)
-embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
-vectorstore = Chroma.from_documents(docs, embedding_function, persist_directory="./chroma_db_nccn")
 
+
+embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
+
+
+vectorstore = Chroma(persist_directory="./chroma_db_nccn", embedding_function=embedding_function)
+
+
+def batch_documents(documents, batch_size=166):
+    for i in range(0, len(documents), batch_size):
+        yield documents[i:i + batch_size]
+
+
+for batch in batch_documents(docs, batch_size=166):
+    vectorstore.add_documents(batch)
+
+
+vectorstore.persist()
 
 
 print(vectorstore._collection.count())
+
 
--- rag.py
+++ rag.py
@@ -1,1 +1,60 @@
-# GEMEINI_API_KEY = "
+import os
+import signal
+import sys
+
+import google.generativeai as genai
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+
+GEMEINI_API_KEY = ""
+
+def signal_handler(sig, frame):
+    print("\n Thanks for using Gemini. :) ")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+def generate_rag_prompt(query, context):
+    escaped = context.replace("'", "").replace('"', "").replace("\n", " ")
+    prompt = ("""
+        You are a helpful and informative bot that answers questions using text from the reference context included below. \
+        Be sure to respond in a complete sentence, being comprehensive, including all relevant background information. \
+        However, you are talking to a non-technical audience, so be sure to break down complicated concepts and \
+        strike a friendly and conversational tone. \
+        If the context is irrelevant to the answer, you may ignore it.
+                QUESTION: '{query}'
+                CONTEXT:'{context}'
+
+
+                ANSWER: 
+                """).format(query=query, context=context)
+
+    return prompt
+
+def get_relevant_context_from_db(query):
+    context = ""
+    embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vector_db = Chroma(persist_directory="./chroma_db_nccn", embedding_function=embedding_function)
+    search_results = vector_db.similarity_search(query, k=6)
+    for result in search_results:
+        context += result.page_content + "\n"
+    return context
+
+def generate_answer(prompt):
+    genai.configure(api_key=GEMEINI_API_KEY)
+    model = genai.GenerativeModel(model_name='gemini-pro')
+    answer = model.generate_content(prompt)
+    return answer.text
+
+
+welcome_text = generate_answer("Can you quickly introduce yourself")
+print(welcome_text)
+
+while True:
+    print("--------------------------------------------------")
+    print("What would you like to ask?")
+    query = input("Query: ")
+    context = get_relevant_context_from_db(query)
+    prompt = generate_rag_prompt(query=query, context=context)
+    answer = generate_answer(prompt=prompt)
+    print(answer)
